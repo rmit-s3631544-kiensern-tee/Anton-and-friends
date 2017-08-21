04ad3b8d6402f21165eca0de0563c2b6b90a7de9
rmit-s3540694-anton-su <s3540694@student.rmit.edu.au>
2017-08-21 15:11:58 +1000

    Test Case update

--- SalesProcessTest.java
+++ SalesProcessTest.java
@@ -11,29 +11,14 @@ public class SalesProcessTest {
 	/*
 	@Test
 	public void ProductIdtest() throws IOException {
-		int id = Salesprocess.main();
+		int id = Salesprocess.sales();
 		assertEquals(2, id);
 	}
 	*/
-	
-	/*
-	@Test
-	public void ProductNametest() throws IOException {
-		String name = Salesprocess.main();
-		assertEquals("2", name);
-	}
-	*/
-	/*
-	@Test
-	public void ProductPricetest() throws IOException {
-		double price = Salesprocess.main();
-		assertEquals(10, price, 0.001);
-	}*/
-	
 	/*
 	@Test
 	public void ProductQuantitytest() throws IOException {
-		int quantity = Salesprocess.main(10);
+		int quantity = Salesprocess.sales();
 		assertEquals(10, quantity);
 	}*/
 }
