1bd4819ff5335e98096d816b36a8e6881c6944bd
rmit-s3631544-kiensern-tee <30815934+rmit-s3631544-kiensern-tee@users.noreply.github.com>
2017-08-21 14:35:00 +1000

    Add files via upload

--- Salesprocess.java
+++ Salesprocess.java
@@ -23,26 +23,9 @@ public class Salesprocess {
 	          {
 	            System.out.print ("Enter the ID of the item: ");
 	            productid = scan.nextInt();
-	            
-	            //System.out.print("Enter the name of the item: ");
-	            //String productname = scan.next();
-
 	            System.out.print ("Enter the quantity: ");
 	            quantity = scan.nextInt();
-	            
-	            //System.out.print ("Enter the unit price: ");
-	            //price = scan.nextDouble();
-
-	            // *** create a new item and add it to the cart
-	            //product temp = new product();
-	            //product product = new product();
-	            //temp = product.readProduct(productid,quantity);
-	           
 	            cart1.addtocart(productid,quantity);
-
-
-
-	            // *** print the contents of the cart object using println
 	            System.out.println(cart1);
 
 	            System.out.print ("Continue shopping (y/n)? ");
