eb37a29ddc95b6bce91d0f123a8a72d235e2237d
rmit-s3631544-kiensern-tee <30815934+rmit-s3631544-kiensern-tee@users.noreply.github.com>
2017-09-03 21:10:42 +1000

    Add files via upload

--- Supplier.java
+++ Supplier.java
@@ -1,68 +1,76 @@
 package supermarketsystem;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
 
 public class Supplier {
 
-private Supplier supplier;
-private int supplierid;
+private Supplier[] supplier = new Supplier[10];
+private String supplierid;
 private String suppliername;
-private Double stocklevel;
+private int stocklevel;
 
 	public Supplier(){
 		
 	}
-	public Supplier(int supplierid, String suppliername, Double stocklevel2){
+	
+	public Supplier(String supplierid, String suppliername, int stocklevel2){
 		this.supplierid = supplierid;
 		this.suppliername = suppliername;
 		this.stocklevel = stocklevel2;
 	}
 	
-	public String getsupplier(int supplierid) throws FileNotFoundException{
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(new File("suppliername.txt"));;
-		List<String> line = new ArrayList<String>();
-		while (sc.hasNextLine()) {
-		  line.add(sc.nextLine());
-		}
-
-		String[] suppliername = line.toArray(new String[0]);
-		
-		@SuppressWarnings("resource")
-		Scanner scan = new Scanner(new File("stocklevel.txt"));
-		List<String> lines = new ArrayList<String>();
-		while (scan.hasNextLine()) {
-			  lines.add(scan.nextLine());
+	public void supplierlist(){
+		supplier[0] = new Supplier("s01", "fresh onions", 100);
+		supplier[1] = new Supplier("s02", "good lady", 100);
+		supplier[2] = new Supplier("s03", "famous amos", 10);
+		supplier[3] = new Supplier("s04", "doritos", 100);
+		supplier[4] = new Supplier("s05", "dutch lady", 100);
+		supplier[5] = new Supplier("s06", "blackcoco", 100);
+		supplier[6] = new Supplier("s07", "caram", 100);
+		supplier[7] = new Supplier("s08", "starbucks", 100);
+		supplier[8] = new Supplier("s09", "krispy kreme", 100);
+		supplier[9] = new Supplier("s10", "moonrice", 100);
+	}
+	
+	
+	public String getsuppliername(String supplierid){
+		supplierlist();
+		String name = null;
+		for (int i = 0; i< supplier.length; i++){
+			if (supplier[i].getID().compareTo(supplierid) == 0){
+				name = supplier[i].getName();
 			}
-
-		String[] stocklevel = lines.toArray(new String[0]);
-		
-		Double [] stock = new Double[stocklevel.length];
-	    for(int i=0 ; i < stocklevel.length; i++){  
-		        stock[i] = Double.parseDouble(stocklevel[i]);  
-	    }
-	    
-	    int available = 0;
-        for(int i = 0 ; i <suppliername.length; i++)
-        {
-        	if (supplierid == i)
-        	{
-        		  this.supplierid = supplierid;
-        		  this.suppliername = suppliername[i];
-        		  this.stocklevel = stock[i];
-        		  available = 1;
-        		  supplier = new Supplier(this.supplierid, this.suppliername, this.stocklevel);
-        		  System.out.println(this.suppliername);
-        		  System.out.println(this.stocklevel);
-        	}
-        }
-        	if(available==0){
-        		System.out.print("Fail");
-        	}
+		}
+		return name;
+	}
+	
+	public String supplyreport(){
+		supplierlist();
+		String contents = "Supply report\n";
+		for (int i = 0; i < supplier.length; i++){
+		      contents += supplier[i].toString() + "\n";
+		      contents += "\n";
+		}
+		return contents;
+	}
+	
+	public String toString()
+	{
+		return String.format("SupplierId : %s\nSupplierName: %s\nStock level : %d\n", 
+				this.supplierid, this.suppliername, this.stocklevel);
+	}
+	
+	public String getID(){
+		return this.supplierid;
+	}
+	
+	public String getName(){
 		return this.suppliername;
 	}
-		
+	
+	public int stocklevel(){
+		return this.stocklevel;
 	}
-
+	
+	
+	
+}
